e22a5b16bd9a32f2f559a57e4557f77d150c3909
hongo <hongobongo@protonmail.com>
2019-03-25 18:17:03 -0400

    added test for section_options

--- test/test_app_factory.py
+++ test/test_app_factory.py
@@ -1,6 +1,7 @@
 import inspect
 import unittest
 from pyquilted.app_factory import *
+from pyquilted.builder.section_factory import SectionOptions
 from pyquilted.quilted.style_options import StyleOptions
 
 
@@ -15,11 +16,13 @@ class TestAppFactory(unittest.TestCase):
 
         self.assertTrue(hasattr(factory, 'args'))
         self.assertTrue(hasattr(factory, 'options'))
+        self.assertTrue(hasattr(factory, 'section_options'))
         self.assertTrue(hasattr(factory, 'style_options'))
 
         self.assertTrue(isinstance(factory.args, dict))
         self.assertTrue(isinstance(factory.style_options, dict))
         self.assertTrue(isinstance(factory.options, AppOptions))
+        self.assertTrue(isinstance(factory.section_options, SectionOptions))
 
         self.assertTrue(inspect.ismethod(factory.create))
 
