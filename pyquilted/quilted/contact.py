eedc77e17cc91cac1fc43cade0ce958b3ffb4b81
hongo <hongobongo@protonmail.com>
2019-04-01 07:20:42 -0400

    Add support for link feature

--- pyquilted/quilted/contact.py
+++ pyquilted/quilted/contact.py
@@ -9,6 +9,7 @@ class EmailContact:
         self.label = 'email'
         self.value = email  # need to validate
         self.icons = icons or ['fa-envelope']
+        self.link = 'mailto:' + email
 
 
 class PhoneContact:
@@ -17,11 +18,15 @@ class PhoneContact:
         self.label = 'phone'
         self.value = phone  # need to validate
         self.icons = icons or ['fa-phone']
+        self.link = 'tel:' + self.to_digits()
+
+    def to_digits(self):
+        return ''.join(i for i in self.value if i.isdigit())
 
 
 class SocialContact:
     """The social part in the contact list"""
-    def __init__(self, handle=None, sites=None, **kwargs):
+    def __init__(self, handle=None, sites=None, link=None, **kwargs):
         """A social contact whose sites will be iconified and grouped
 
         Args:
@@ -37,6 +42,7 @@ class SocialContact:
         self.label = 'social'
         self.value = handle
         self.icons = self._iconify(sites)
+        self.link = link
 
     def _iconify(self, sites):
         """appends fa- to list of sites to create the font awesome
