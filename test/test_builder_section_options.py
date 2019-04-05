43de24e57757442ea5816b66f3ebe68bc80b8b59
hongo <hongobongo@protonmail.com>
2019-04-05 14:47:36 -0400

    Update and New test cases for skills table

--- test/test_builder_section_options.py
+++ test/test_builder_section_options.py
@@ -5,15 +5,18 @@ from pyquilted.builder.section_factory import SectionOptions
 class TestSectionOptions(unittest.TestCase):
     def setUp(self):
         self.options_default = SectionOptions()
-        self.options = SectionOptions(heading='compact')
+        self.options = SectionOptions(heading='compact', skills_table=True)
 
     def test_section_options_default(self):
-        self.assertIsNotNone(self.options_default.heading)
+        self.assertTrue(hasattr(self.options_default, 'heading'))
+        self.assertTrue(hasattr(self.options_default, 'skills_table'))
+
         self.assertEqual(self.options_default.heading, 'auto')
+        self.assertEqual(self.options_default.skills_table, False)
 
     def test_section_options(self):
-        self.assertIsNotNone(self.options.heading)
         self.assertEqual(self.options.heading, 'compact')
+        self.assertTrue(self.options.skills_table)
 
 
 if __name__ == '__main__':
--- test/test_builder_skills.py
+++ test/test_builder_skills.py
@@ -1,24 +1,28 @@
 import unittest
 from pyquilted.builder.skills import SkillsBuilder
-from pyquilted.yaml_loader import YamlLoader
+from pyquilted.quilted.skills import Skills
+from pyquilted.quilted.skills_table import SkillsTable
 
 
 class TestBuilderSkills(unittest.TestCase):
-    def test_mapper_skills(self):
-        with open('test/validations/skills.yml') as f:
-            data = YamlLoader.ordered_load(f)
-        mapper = SkillsBuilder(data['skills'])
-        skills = mapper.deserialize()
+    def setUp(self):
+        skills = ["karate", "joust", "parkour", "spooning", "youtubing", "dev"]
+        self.builder = SkillsBuilder(skills)
+        self.builder_table = SkillsBuilder(skills, table=True)
 
-        valid = {
-                "skills": {
-                    "label": "Skills",
-                    "value": "Bravery, Leadership, Swordsmanship, "
-                             "Brooding, Knowing-nothing, HTML",
-                    "icon": "fa-wrench"
-                    }
-                }
-        self.assertEqual(skills.serialize(), valid)
+    def test_builder_skills(self):
+        self.assertTrue(hasattr(self.builder, 'odict'))
+        self.assertTrue(hasattr(self.builder, 'table'))
+
+        self.assertFalse(self.builder.table)
+        self.assertTrue(self.builder_table.table)
+        self.assertIsInstance(self.builder.odict, list)
+
+    def test_builder_deserialize(self):
+        skills_default = self.builder.deserialize()
+        skills_x = self.builder_table.deserialize()
+        self.assertIsInstance(skills_default, Skills)
+        self.assertIsInstance(skills_x, SkillsTable)
 
 
 if __name__ == '__main__':
